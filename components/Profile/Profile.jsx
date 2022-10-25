54a5dbd621405e2a0d33f5a4d583a265a4ceaa5c
Esinnation <abdulrazaqridollah96@gmail.com>
2022-10-25 05:00:59 -0700

    feat: Create profile.css and add profile image

---
+++ components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Profile.css"
+function Profile(){
+    return(
+        <div>
+            <img src="../public\Images\profile.jpg" alt="" id="profile__img"/>
+            
+        </div>
+    )
+}
+
+export default Profile
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Profile from '../components/Profile/Profile'
 
 function App() {
 
   return (
     <div className="App">
+      <Profile />
     </div>
   )
 }
