2a5153df2d345fced9877524e27efb6bb3edf688
Esinnation <abdulrazaqridollah96@gmail.com>
2022-10-26 09:04:38 -0700

    bug: fix image not displaying

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -4,8 +4,8 @@ function Profile(){
     return(
         <div className="Profile">
 
-            <img src="../Images\share.svg" alt="" id="share_btn"/>
-            <img src="../Images\profile.jpg" alt="" id="profile__img"/>
+            <img src="/Images\share.svg" alt="" id="share_btn"/>
+            <img src="/Images\profile.jpg" alt="" id="profile__img"/>
             <p id="twitter">Esinnation</p>
             <p id="slack">Esinnation</p>
         </div>
